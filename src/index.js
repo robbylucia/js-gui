73739e2f86788e336c846706cb4b090b08ed4d2d
Robby Lucia <Robby.Lucia@timeinc.com>
2018-12-11 14:53:30 -0500

    FEAT: webpack config and temp debug msg

--- src/index.js
+++ src/index.js
@@ -107,7 +107,7 @@ const el = (tag, attr = {}, ...children) => {
     }
 
     const a = child => append(node, child);
-
+    console.log("Attributes:", attr);
     children.forEach(a);
 
     return node;
@@ -278,4 +278,4 @@ export default {
 
 export { round, bootstrapify, add, h1, h2, h3, h4, h5, h6, div };
 
-// add(h1("hiya"))
+add(h5("jsgui debug: " + Math.round(Math.random() * 100), { style: "box-shadow: 0 0 100px 0px #b9d854; position: fixed; top: 0; right: 0; padding: 0.5em; background: #282828; color: #BADA55" }))
