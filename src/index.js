41cdb88ae42fc83ded012c1f0fd7bcbcfaf7e3b8
Robby Lucia <Robby.Lucia@timeinc.com>
2018-12-13 13:07:22 -0500

    prestash

--- src/index.js
+++ src/index.js
@@ -246,3 +246,4 @@ export {jsgui};
 // dev only below
 jsgui.add(jsgui.h5("jsgui debug v1: " + Math.round(Math.random() * 100), { style: "box-shadow: 0 0 100px 0px #b9d854; position: fixed; top: 0; right: 0; padding: 0.5em; background: #282828; color: #BADA55" }))
 jsgui.add(jsgui.img("https://picsum.photos/400/400/?random", "Random test image"))
+jsgui.add(jsgui.h5("testaroo"))
